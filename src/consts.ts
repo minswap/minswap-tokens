a159f70cb5a529f3130c7d997961981b46befb73
rinchan01 <thailinh8426@gmail.com>
2025-03-18 01:47:29 +0700

    add validation on address

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
 export const TOKENS_DIR = "tokens";
 export const URL_REGEX = "^https://.*$";
-export const ADDRESS_REGEX = "^(addr|stake)[0-9a-zA-Z]+";
+export const ADDRESS_REGEX = "^(addr1|stake1)[0-9a-zA-Z]{53}$|^(addr1|stake1)[0-9a-zA-Z]{98}$|^(Ddz|Ae2)[0-9a-zA-Z]+$";
 export const ASSET_ID_REGEX = "^(([a-fA-F0-9]{2}){28,})$";
